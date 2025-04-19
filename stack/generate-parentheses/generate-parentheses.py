746f5703a1271de7c1f92c7473e41ba378bbf2e0
Hassnae22 <hassnalaoui21@gmail.com>
2025-04-19 17:04:11 +0200

    add generate parentheses prob with backtrack

---
+++ stack/generate-parentheses/generate-parentheses.py
@@ -0,0 +1,17 @@
+def generateParenthesis(n):
+    result = []
+    
+    def backtrack(current_string, open, close):
+        if len(current_string) == 2*n:
+            result.append(current_string)
+            return
+        
+        if open < n:
+            backtrack(current_string + '(', open + 1, close)
+
+        if close < open:
+            backtrack(current_string + ')', open, close + 1)
+     
+    backtrack('', 0, 0)
+
+    return result
