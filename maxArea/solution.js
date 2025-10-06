5f77fd442410dac441b1d68351cd7743fdfbb7c9
Hassnae22 <hassnalaoui21@gmail.com>
2025-10-06 10:06:35 +0200

    add js and py solution for max area

---
+++ maxArea/solution.js
@@ -0,0 +1,26 @@
+var maxArea = function(height) {
+
+    const left = 0
+    const right = height.length - 1
+    let maxArea = 0
+    while(left < right){
+         let leftValue = height[left]
+         let righValue = height[right]
+         if (leftValue < righValue){
+            let currentMax = leftValue * (right - left)
+            if (maxArea < currentMax)
+                maxArea = currentMax;
+            left++;
+         }
+         else{
+            let currentMax = righValue * (right - left)
+            if(maxArea < currentMax)
+                maxArea = currentMax
+            right++;
+         }
+
+    }
+    return maxArea;
+
+    
+};
